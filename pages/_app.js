f6fb4e71a48606330dba76efcbb751924679be1a
Biplop Maharjan <maharjankija@gmail.com>
2026-04-06 17:49:38 +0545

    version 2.3

--- pages/_app.js
+++ pages/_app.js
@@ -6,10 +6,6 @@ export default function App({ Component, pageProps }) {
   if (pageProps.adminPage) {
     return <Component {...pageProps} />
   }
-  // Some pages (like the legacy single-page homepage) render their own full layout
-  if (pageProps.noLayout) {
-    return <Component {...pageProps} />
-  }
   return (
     <Layout>
       <Component {...pageProps} />
